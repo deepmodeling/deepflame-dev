9d92a30c35455ea05826d2bc6e3c48db3071da3f
JX278 <dorisdqcie2018@163.com>
2022-09-21 11:13:19 +0800

    0D Test

--- test/corrtest.cpp
+++ test/corrtest.cpp
@@ -2,40 +2,43 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include "readmaxT.h"
-#include "readmidT.h"
 #include <ostream>
 #include <filesystem>
+using namespace std;
 
-using std::filesystem::current_path;
+float readmidTH2();
+float readmaxTH2();
+float readmidTCH4();
+float readmaxTCH4();
 
-float readmidT();
-float readmaxT();
+
+float H2maxT = readmaxTH2();
+float H2midT = readmidTH2();
+float CH4maxT = readmaxTCH4();
+float CH4midT = readmidTCH4();
 
 
-float a = readmaxT();
-float b = readmidT();
-
-TEST(corrtest,df0DFoam_max){
-    EXPECT_FLOAT_EQ(a,2588.88);
+TEST(corrtest,df0DFoam_H2){
+    EXPECT_FLOAT_EQ(H2maxT,2588.88);   // compare the maximum temperature of H2 case 
+    EXPECT_FLOAT_EQ(H2midT,1298.12); // compare the temperature of H2 case at the maximum gradient when t = 0.000245s
 }
 
-TEST(corrtest,df0DFoam_mid){
-    EXPECT_FLOAT_EQ(b,2007.72);
+TEST(corrtest,df0DFoam_CH4){
+    EXPECT_FLOAT_EQ(CH4maxT,2816.82);   // compare the maximum temperature of CH4 case 
+    EXPECT_FLOAT_EQ(CH4midT,2410.39); // compare the temperature of CH4 case at the maximum gradient when t = 0.000249s
 }
 
 
-float readmaxT(){
-    string str1 = get_current_dir_name();
-    string str2 = "T";
+
+float readmaxTH2(){
     float a;
-    string inFileName = "T" ;
+    string inFileName = "0DH2/T" ;
     ifstream inFile;
     inFile.open(inFileName.c_str());
 
     if (inFile.is_open())  
     {
-        inFile.ignore(100);
+        inFile.ignore(80);
         while (inFile >> a){
        }
     
@@ -47,22 +50,71 @@ float readmaxT(){
     return a;
 }
 
-float readmidT(){
-    
+float readmaxTCH4(){
     float a;
-    float b;
-    int i = 0;
-    
-    string inFileName = "T";
+    string inFileName = "0DCH4/T" ;
     ifstream inFile;
     inFile.open(inFileName.c_str());
 
     if (inFile.is_open())  
     {
-        inFile.ignore(100);
+        inFile.ignore(80);
+        while (inFile >> a){
+       }
+    
+    }
+    else { //Error message
+        cerr << "Can't find input file " << inFileName << endl;
+    }
+    cout << a << endl;
+    return a;
+}
+
+float readmidTH2(){
+    
+    float a;
+    float b;
+    int i = 0;
+    
+    string inFileName = "0DH2/T";
+    ifstream inFile;
+    inFile.open(inFileName.c_str());
+
+    if (inFile.is_open())  
+    {
+        inFile.ignore(80);
         while (inFile >> a){
             i ++ ;
-            if (i == 601){
+            if (i == 490 ){  // t = 0.000245 dt = 37.25, maximum gradient
+                b = a;
+            }
+        }
+    
+    }
+    else { //Error message
+        cerr << "Can't find input file " << inFileName << endl;
+    }
+
+    return b;
+}
+
+
+float readmidTCH4(){
+    
+    float a;
+    float b;
+    int i = 0;
+    
+    string inFileName = "0DCH4/T";
+    ifstream inFile;
+    inFile.open(inFileName.c_str());
+
+    if (inFile.is_open())  
+    {
+        inFile.ignore(80);
+        while (inFile >> a){
+            i ++ ;
+            if (i == 498 ){  // t = 0.000249 dt = 84.165, maximum gradient
                 b = a;
             }
         }
