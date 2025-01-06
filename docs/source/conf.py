0a1d64ca38becf515b2d75284d8c7d7458145102
chenzhenyi-123 <sy2115303@buaa.edu.cn>
2025-01-06 10:53:52 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 extensions = ["deepmodeling_sphinx",
     'sphinx_rtd_theme',
     'sphinx.ext.duration',
