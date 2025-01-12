0c77d7daa157343e681e87aa1aca4720d620b154
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-01-12 11:14:01 +0800

    switch to sphinx_book_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ intersphinx_disabled_domains = ['std']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_logo = 'DeepFlameLOGO.png'
 
 
