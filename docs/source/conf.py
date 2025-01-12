02e45b33fa4c933a24b7b3233ab5a33c375797af
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2025-01-12 11:16:03 +0800

    Remove unused import in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sphinx_rtd_theme
 extensions = ["deepmodeling_sphinx",
     'sphinx_rtd_theme',
     'sphinx.ext.duration',
